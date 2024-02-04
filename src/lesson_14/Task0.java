d1b52e59e777fe9d7c5a71c5c3a67e7d316bce19
Vladyslav <nozhik09@gmail.com>
2024-02-04 21:55:03 +0100

    homeWork

---
+++ src/lesson_14/Task0.java
@@ -0,0 +1,27 @@
+package lesson_14;
+
+public class Task0 {
+    public static void main(String[] args) {
+        String str = "a b c d e f g";
+        upperCase(str);
+    }
+
+    public static void upperCase(String str) {
+
+
+        for (int i = 0; i <str.length() ; i++) {
+            char c = str.charAt(i);
+            if (Character.isLowerCase(c)){
+                c-=32;
+            }
+            System.out.print(c);
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ src/lesson_14/Task1.java
@@ -0,0 +1,31 @@
+package lesson_14;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+int[] array = {1,2,3,4,5,6};
+int i = 1;
+int j = 5;
+swap(array , i, j);
+
+
+
+
+
+    }
+
+public static void swap (int[ ] array , int i , int j){
+int temp = array[i] ;
+array[i]=array[j];
+array[j] = temp;
+
+    for (int k = 0; k < array.length; k++) {
+        System.out.print(array[k] +" , ");
+    }
+
+
+}
+
+
+
+}
