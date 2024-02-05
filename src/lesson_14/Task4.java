930798390a56ed097d824ca62b87812ca093512b
Vladyslav <nozhik09@gmail.com>
2024-02-05 14:51:16 +0100

    homeWork

---
+++ src/lesson_14/Task4.java
@@ -0,0 +1,26 @@
+package lesson_14;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String str  = " Hello" ;
+String second = fristAndSecondWords(str);
+
+        System.out.println(str);
+        System.out.println(second);
+    }
+
+
+
+public static String fristAndSecondWords(String str){
+        if (str.length()<3) return "";
+
+
+
+    char secondWords = Character.toUpperCase(str.charAt(2));
+    char thirdWords = Character.toUpperCase(str.charAt(3));
+
+
+return String.valueOf(secondWords) + thirdWords ;
+
+}
+}
