b41dbdd3b6861f203bfd749c4dc9c50af7536878
Vladyslav <nozhik09@gmail.com>
2024-02-05 14:52:31 +0100

    homeWork

--- src/lesson_14/Task4.java
+++ src/lesson_14/Task4.java
@@ -2,25 +2,23 @@ package lesson_14;
 
 public class Task4 {
     public static void main(String[] args) {
-        String str  = " Hello" ;
-String second = fristAndSecondWords(str);
+        String str = "Hello";
+        String second = fristAndSecondWords(str);
 
         System.out.println(str);
         System.out.println(second);
     }
 
 
-
-public static String fristAndSecondWords(String str){
-        if (str.length()<3) return "";
-
+    public static String fristAndSecondWords(String str) {
+        if (str.length() < 3) return "";
 
 
-    char secondWords = Character.toUpperCase(str.charAt(2));
-    char thirdWords = Character.toUpperCase(str.charAt(3));
+        char secondWords = Character.toUpperCase(str.charAt(1));
+        char thirdWords = Character.toUpperCase(str.charAt(2));
 
 
-return String.valueOf(secondWords) + thirdWords ;
+        return String.valueOf(secondWords) + thirdWords;
 
-}
+    }
 }
